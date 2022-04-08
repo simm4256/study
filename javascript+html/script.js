13e28e329a90c7d2b2ca8f856567855ac1853654
simm4256 <simm4256@naver.com>
2022-04-09 01:23:27 +0900

    study

--- javascript+html/script.js
+++ javascript+html/script.js
@@ -1,29 +1,12 @@
-class MyStr {
-    #s = '';
-    constructor(s) {
-        this.#s = s;
-    }
-    set s(str) {
-        this.#s = str;
-    }
-    get s() {
-        return this.#s;
-    }
-    changeByIndex(startIndex, lastIndex, newStr) {
-        this.#s = this.#s.slice(0, startIndex) + newStr + this.#s.slice(lastIndex);
-    }
-}
-
-function* fn() {
-    console.log(1);
-    yield;
-    let x = yield "숫자를 입력하세요 : ";
-    console.log(x);
-    console.log(3);
-    return;
-}
-
-let a = fn();
-a.next();
-a.next();
-a.next(4);
+button1.addEventListener('click', (e) => {
+    alert(1);
+});
+button1.addEventListener('click', (e) => {
+    alert(2);
+})
+button1.addEventListener('mouseover', (e) => {
+    e.target.style.backgroundColor = "yellow";
+})
+button1.addEventListener('mouseleave', (e) => {
+    e.target.style.backgroundColor = 'white';
+})
